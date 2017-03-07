32aed45e820ac7331cee12c5c57d3ddc7a0cda29
shugo kajita <kaj1m5.gon@gmail.com>
2017-03-07 21:11:00 +0000

    add file validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,15 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :image_size
   
   mount_uploader :image, ImageUploader
+
+  private
+  def image_size
+    if image.size > 1.megabytes
+      errors.add(:image, "1MB以下にして下さい")
+    end
+  end
+  
 end
